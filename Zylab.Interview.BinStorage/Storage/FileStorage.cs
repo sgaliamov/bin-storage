87448cf3adc084259b20dca64135af9aa7222a32
s. galyamov <sgalyamov@gmail.com>
2016-02-25 15:12:29 +0300

    FileStorage sketch

--- Zylab.Interview.BinStorage/Storage/FileStorage.cs
+++ Zylab.Interview.BinStorage/Storage/FileStorage.cs
@@ -1,25 +1,51 @@
 ﻿using System.IO;
+using System.Security.Cryptography;
 using Zylab.Interview.BinStorage.Index;
 
 namespace Zylab.Interview.BinStorage.Storage {
 
 	public class FileStorage : IStorage {
-		private readonly string _storageFilePath;
-
+		private const int DefaultBufferSize = 4096;
+		private readonly Stream _reader;
+		private readonly Stream _writer;
 		public FileStorage(string storageFilePath) {
-			_storageFilePath = storageFilePath;
+			_writer = new FileStream(storageFilePath, FileMode.Append, FileAccess.Write, FileShare.Read);
+			_reader = new FileStream(storageFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
 		}
 
-		public void Dispose() {
-			throw new System.NotImplementedException();
-		}
 
 		public IndexData Append(Stream data) {
-			throw new System.NotImplementedException();
+			var indexData = new IndexData {
+				Offset = _writer.Position
+			};
+
+			var buffer = new byte[DefaultBufferSize];
+			using(var md5 = MD5.Create()) {
+				int read;
+				while((read = data.Read(buffer, 0, buffer.Length)) > 0) {
+					_writer.Write(buffer, 0, read);
+
+					md5.TransformBlock(buffer, 0, read, buffer, 0);
+				}
+				md5.TransformFinalBlock(buffer, 0, buffer.Length);
+				indexData.Md5Hash = md5.Hash;
+			}
+
+			indexData.Size = _writer.Position - indexData.Offset;
+
+			return indexData;
 		}
 
 		public Stream Get(IndexData indexData) {
-			throw new System.NotImplementedException();
+			_reader.Seek(indexData.Offset, SeekOrigin.Begin);
+
+			return new BufferedStream(_reader);
+		}
+
+		public void Dispose() {
+			// todo: https://msdn.microsoft.com/en-us/library/system.idisposable(v=vs.110).aspx
+			_writer.Dispose();
+			_reader.Dispose();
 		}
 	}
 
